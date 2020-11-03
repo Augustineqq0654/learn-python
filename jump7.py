7880f996237969c80925263582f10b8d9d8a18a1
KeZihao <15927241986@163.com>
2020-11-03 20:36:05 +0800

    add jump7 code

--- jump7.py
+++ jump7.py
@@ -1,8 +1,9 @@
+# 1~100中逢7跳过
 num = 1
 while num <101:
-	if num % 7 != 0:
-		if num%10 != 7:
-			if num//10 != 7:
+	if num % 7 != 0:            #不为7的倍数
+		if num%10 != 7:         #个位数不为7
+			if num//10 != 7:    #十位数不为7
 				print(num)
 	num += 1
 
